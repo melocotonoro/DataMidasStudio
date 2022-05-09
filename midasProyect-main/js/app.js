183e87fe7928bb316fe5a7db0ef979efc3f3f795
Melisa Oro <84943599+melocotonoro@users.noreply.github.com>
2022-05-09 15:25:00 -0300

    Add files via upload

--- midasProyect-main/js/app.js
+++ midasProyect-main/js/app.js
@@ -1,3 +1,43 @@
+window.onload=()=>{
+    animationNavBar();
+}
+
+function animationNavBar(){
+$("sectionNavBar").hide();
+
+$("sectionNavBar").prepend(`<nav class="containerItemsNav">
+<div> <p class="logo">MIDAS</p>
+    <P class="logo">DATA STUDIO</P>
+</div>
+<button class="menuIcon">
+    <i class="fa-solid fa-bars"></i>  
+</button>
+<ul class="itemsNavBar itemsNavBar-visible">
+    <li>
+    <a href="#">Nosotros</a>
+    </li>
+    <li>
+    <a href="#">Nuestros servicios</a>
+    </li>
+    <li>
+    <a href="#">Trabajos realizados</a>
+    </li>
+    <li>
+        <a href="#">¿Que dicen?</a>
+    </li>
+    <li>
+    <a href="#">Trabajá con nosotros</a>
+    </li>
+    <li>
+        <a href="#">Contacto</a>
+    </li>
+</ul>
+</nav>`);
+
+$("sectionNavBar").fadeIn(3000);
+}
+
+
 // NAV BAR
 const buttomNav= document.querySelector(".menuIcon")
 const navList= document.querySelector(".itemsNavBar")
